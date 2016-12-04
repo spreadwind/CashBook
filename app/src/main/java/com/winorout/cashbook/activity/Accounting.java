f98972c649c6cf94e8e438b75503e76dee3f69ad
wanxuedong <2381144912@qq.com>
2016-12-04 11:13:49 +0800

    框架搭建

---
+++ app/src/main/java/com/winorout/cashbook/activity/Accounting.java
@@ -0,0 +1,18 @@
+package com.winorout.cashbook.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.winorout.cashbook.R;
+
+/**
+ * Created by Mr-x on 2016/12/04.
+ */
+
+public class Accounting extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accounting);
+    }
+}
