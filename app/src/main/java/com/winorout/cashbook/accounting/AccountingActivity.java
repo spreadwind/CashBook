87768d00a24ff1cd9bc5442cda3e804567be664d
zhangminvip <2107609379@qq.com>
2016-12-12 23:56:53 +0800

    记账功能第二次更新

--- app/src/main/java/com/winorout/cashbook/accounting/AccountingActivity.java
+++ app/src/main/java/com/winorout/cashbook/accounting/AccountingActivity.java
@@ -5,29 +5,51 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.winorout.cashbook.R;
 
 /**
- * Created by Mr-x on 2016/12/04.
+ * Created by ZhangMin on 2016/12/04.
  */
 
 public class AccountingActivity extends Activity implements View.OnClickListener {
     private ExpenseFragment expenseFragment = new ExpenseFragment();
     private IncomeFragment incomeFragment = new IncomeFragment();
 
-    private View income_btn;
-    private View expense_btn;
-    private Button back_btn;
+    private LinearLayout income_btn;
+    private LinearLayout expense_btn;
+    private LinearLayout back_btn;
     private Button clean_btn;
-    private TextView type_amount;
+    private Button one_btn;
+    private Button two_btn;
+    private Button three_btn;
+    private Button four_btn;
+    private Button five_btn;
+    private Button six_btn;
+    private Button seven_btn;
+    private Button eight_btn;
+    private Button nine_btn;
+    private Button zero_btn;
+    private Button dot_btn;
+    private Button add_btn;
+    private Button minus_btn;
+    private TextView type_amount;   // 计算结果
+    private double type_acount_init = 0.0;   // 计算过程需要用到的结果，最近一个符号后的结果，而不是当前的结果
+    private TextView type_calculate;
+    private int add_time = 0;           //假发的次数
+    private int minus_time = 0;
+    private int or = 0;     //判断加法或者减法
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_accounting);
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction transaction0 = fragmentManager.beginTransaction();
@@ -40,11 +62,25 @@ public class AccountingActivity extends Activity implements View.OnClickListener
     }
 
     public void initView() {
-        type_amount = (TextView)findViewById(R.id.type_amount);
+        zero_btn = (Button) findViewById(R.id.zero_btn);
+        add_btn = (Button) findViewById(R.id.add_btn);
+        minus_btn = (Button) findViewById(R.id.minus_btn);
+        one_btn = (Button) findViewById(R.id.one_btn);
+        two_btn = (Button) findViewById(R.id.two_btn);
+        three_btn = (Button) findViewById(R.id.three_btn);
+        four_btn = (Button) findViewById(R.id.four_btn);
+        five_btn = (Button) findViewById(R.id.five_btn);
+        six_btn = (Button) findViewById(R.id.six_btn);
+        seven_btn = (Button) findViewById(R.id.seven_btn);
+        eight_btn = (Button) findViewById(R.id.eight_btn);
+        nine_btn = (Button) findViewById(R.id.nine_btn);
+        dot_btn = (Button) findViewById(R.id.dot_btn);
+        type_amount = (TextView) findViewById(R.id.type_amount);
+        type_calculate = (TextView) findViewById(R.id.type_calculate);
         clean_btn = (Button) findViewById(R.id.clean_btn);
-        income_btn = findViewById(R.id.income_layout);
-        expense_btn = findViewById(R.id.expense_layout);
-        back_btn = (Button) findViewById(R.id.back_btn);
+        income_btn = (LinearLayout) findViewById(R.id.income_layout);
+        expense_btn = (LinearLayout) findViewById(R.id.expense_layout);
+        back_btn = (LinearLayout) findViewById(R.id.back_btn);
     }
 
 
@@ -53,41 +89,328 @@ public class AccountingActivity extends Activity implements View.OnClickListener
         income_btn.setOnClickListener(this);
         expense_btn.setOnClickListener(this);
         back_btn.setOnClickListener(this);
+        add_btn.setOnClickListener(this);
+        minus_btn.setOnClickListener(this);
+        one_btn.setOnClickListener(this);
+        two_btn.setOnClickListener(this);
+        three_btn.setOnClickListener(this);
+        four_btn.setOnClickListener(this);
+        five_btn.setOnClickListener(this);
+        six_btn.setOnClickListener(this);
+        seven_btn.setOnClickListener(this);
+        eight_btn.setOnClickListener(this);
+        nine_btn.setOnClickListener(this);
+        zero_btn.setOnClickListener(this);
+        add_btn.setOnClickListener(this);
+        minus_btn.setOnClickListener(this);
+        dot_btn.setOnClickListener(this);
+
     }
+
+
+    // 计算器加法方法
+    public void add() {
+        try {
+            String[] i = type_calculate.getText().toString().split("\\+");
+            Double y = Double.parseDouble(i[add_time]);
+            type_amount.setText(String.valueOf(type_acount_init + y));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "用户非法操作", Toast.LENGTH_SHORT).show();
+            clear();
+        }
+
+    }
+
+    // 计算器减法方法
+    public void minus() {
+        try {
+            String[] i = type_calculate.getText().toString().split("\\-");
+            Double y = Double.parseDouble(i[minus_time]);
+            type_amount.setText(String.valueOf(type_acount_init - y));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "用户非法操作", Toast.LENGTH_SHORT).show();
+            clear();
+        }
+    }
+
 
     @Override
     public void onClick(View v) {
+        try {
+            switch (v.getId()) {
+                case R.id.income_layout:
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    transaction.replace(R.id.classify, incomeFragment);
+                    transaction.show(incomeFragment);
+                    transaction.hide(expenseFragment);
+                    transaction.commit();
+                    break;
+
+                case R.id.expense_layout:
+                    FragmentTransaction transaction1 = getFragmentManager().beginTransaction();
+                    transaction1.replace(R.id.classify, expenseFragment);
+                    transaction1.show(expenseFragment);
+                    transaction1.hide(incomeFragment);
+                    transaction1.commit();
+                    break;
+                case R.id.back_btn:
+                    finish();
+                    break;
+
+                case R.id.clean_btn:
+                    clear();
+                    add_time = 0;
+                    minus_time = 0;
+                    type_acount_init = 0.0;
+
+                    break;
+
+                case R.id.one_btn:
+//                if(add_time == 0){
+//                    type_amount.append("1");
+//                }
+//                else if(add_time == 1){
+//                    type_calculate.append("1");
+////                    String i = type_calculate.getText().subSequence(0,3).toString();
+////                    Integer.parseInt(i);
+//                    String[] i = type_calculate.getText().toString().split("\\+");
+//                    int y = Integer.parseInt(i[0])+Integer.parseInt(i[1]);
+//                    type_amount.setText(String.valueOf(y));
+//
+//
+//                }else if(add_time == 2){
+//                    type_calculate.append("1");
+////                    String i = type_calculate.getText().subSequence(0,3).toString();
+////                    Integer.parseInt(i);
+//                    String[] i = type_calculate.getText().toString().split("\\+");
+//                    int y = Integer.parseInt(i[0])+Integer.parseInt(i[1])+Integer.parseInt(i[2]);
+//                    type_amount.setText(String.valueOf(y));
+//                }
+
+                    add_btn.setClickable(true);
+                    minus_btn.setClickable(true);
+                    dot_btn.setClickable(true);
+                    zero_btn.setClickable(true);
+                    if (add_time == 0 && minus_time == 0) {
+                        type_amount.append("1");
+                    } else if (or == 0) {
+                        type_calculate.append("1");
+                        add();
+                    } else {
+                        type_calculate.append("1");
+                        minus();
+                    }
+                    break;
 
 
-        switch (v.getId()) {
-            case R.id.income_layout:
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                transaction.replace(R.id.classify, incomeFragment);
-                transaction.show(incomeFragment);
-                transaction.hide(expenseFragment);
-                transaction.commit();
-                break;
-
-            case R.id.expense_layout:
-                FragmentTransaction transaction1 = getFragmentManager().beginTransaction();
-                transaction1.replace(R.id.classify, expenseFragment);
-                transaction1.show(expenseFragment);
-                transaction1.hide(incomeFragment);
-                transaction1.commit();
-                break;
-            case R.id.back_btn:
-                finish();
-                break;
-
-            case R.id.clean_btn:
-                type_amount.setText("0.00");
-                break;
+                case R.id.two_btn:
+                    add_btn.setClickable(true);
+                    minus_btn.setClickable(true);
+                    dot_btn.setClickable(true);
+                    zero_btn.setClickable(true);
+                    if (add_time == 0 && minus_time == 0) {
+                        type_amount.append("2");
+                    } else if (or == 0) {
 
 
+                        type_calculate.append("2");
+                        add();
+                    } else {
+                        type_calculate.append("2");
+                        minus();
+                    }
+                    break;
+
+                case R.id.three_btn:
+                    add_btn.setClickable(true);
+                    minus_btn.setClickable(true);
+                    dot_btn.setClickable(true);
+                    zero_btn.setClickable(true);
+
+                    if (add_time == 0 && minus_time == 0) {
+                        type_amount.append("3");
+                    } else if (or == 0) {
+                        type_calculate.append("3");
+                        add();
+                    } else {
+                        type_calculate.append("3");
+                        minus();
+                    }
+
+                    break;
 
 
-            default:
-                break;
+                case R.id.four_btn:
+                    add_btn.setClickable(true);
+                    minus_btn.setClickable(true);
+                    dot_btn.setClickable(true);
+                    zero_btn.setClickable(true);
+
+                    if (add_time == 0 && minus_time == 0) {
+                        type_amount.append("4");
+                    } else if (or == 0) {
+                        type_calculate.append("4");
+                        add();
+                    } else {
+                        type_calculate.append("4");
+                        minus();
+                    }
+
+                    break;
+
+                case R.id.five_btn:
+                    add_btn.setClickable(true);
+                    minus_btn.setClickable(true);
+                    dot_btn.setClickable(true);
+                    zero_btn.setClickable(true);
+
+                    if (add_time == 0 && minus_time == 0) {
+                        type_amount.append("5");
+                    } else if (or == 0) {
+                        type_calculate.append("5");
+                        add();
+                    } else {
+                        type_calculate.append("5");
+                        minus();
+                    }
+                    break;
+
+
+                case R.id.six_btn:
+                    add_btn.setClickable(true);
+                    minus_btn.setClickable(true);
+                    dot_btn.setClickable(true);
+                    zero_btn.setClickable(true);
+
+                    if (add_time == 0 && minus_time == 0) {
+                        type_amount.append("6");
+                    } else if (or == 0) {
+                        type_calculate.append("6");
+                        add();
+                    } else {
+                        type_calculate.append("6");
+                        minus();
+                    }
+
+                    break;
+
+                case R.id.seven_btn:
+                    add_btn.setClickable(true);
+                    minus_btn.setClickable(true);
+                    dot_btn.setClickable(true);
+                    zero_btn.setClickable(true);
+
+                    if (add_time == 0 && minus_time == 0) {
+                        type_amount.append("7");
+                    } else if (or == 0) {
+                        type_calculate.append("7");
+                        add();
+                    } else {
+                        type_calculate.append("7");
+                        minus();
+                    }
+                    break;
+
+                case R.id.eight_btn:
+                    add_btn.setClickable(true);
+                    minus_btn.setClickable(true);
+                    dot_btn.setClickable(true);
+                    zero_btn.setClickable(true);
+
+                    if (add_time == 0 && minus_time == 0) {
+                        type_amount.append("8");
+                    } else if (or == 0) {
+                        type_calculate.append("8");
+                        add();
+                    } else {
+                        type_calculate.append("8");
+                        minus();
+                    }
+
+                    break;
+
+
+                case R.id.nine_btn:
+                    add_btn.setClickable(true);
+                    minus_btn.setClickable(true);
+                    dot_btn.setClickable(true);
+                    zero_btn.setClickable(true);
+
+                    if (add_time == 0 && minus_time == 0) {
+                        type_amount.append("9");
+                    } else if (or == 0) {
+                        type_calculate.append("9");
+                        add();
+
+                    } else {
+                        type_calculate.append("9");
+                        minus();
+                    }
+
+                    break;
+
+                case R.id.zero_btn:
+                    if (type_amount.getText().toString().length() == 0) {
+                        zero_btn.setClickable(false);
+                        break;
+                    } else if (add_time == 0 && minus_time == 0) {
+                        type_amount.append("0");
+                    } else if (or == 0) {
+                        type_calculate.append("0");
+                        add();
+                    } else {
+                        type_calculate.append("0");
+                        minus();
+                    }
+
+                    break;
+
+                case R.id.dot_btn:
+                    if (type_amount.getText().toString().length() == 0) {
+                        dot_btn.setClickable(false);
+                        break;
+                    } else if (add_time == 0 && minus_time == 0) {
+                        type_amount.append(".");
+                    } else {
+                        type_calculate.append(".");
+                    }
+
+                    break;
+
+                case R.id.add_btn:
+                    if (type_amount.getText().toString().length() == 0) {
+                        add_btn.setClickable(false);
+                        break;
+                    } else if (add_time == 0 && minus_time == 0) {
+                        type_calculate.append(type_amount.getText().toString() + "+");
+                    } else {
+                        type_calculate.append("+");
+                    }
+                    add_time++;
+                    or = 0;
+
+                    type_acount_init = Double.parseDouble(type_amount.getText().toString());
+                    break;
+
+                case R.id.minus_btn:
+
+                    if (type_amount.getText().toString().length() == 0) {
+                        minus_btn.setClickable(false);
+                        break;
+                    } else if (add_time == 0 && minus_time == 0) {
+                        type_calculate.append(type_amount.getText().toString() + "-");
+                    } else {
+                        type_calculate.append("-");
+                    }
+                    minus_time++;
+                    or = 1;
+                    type_acount_init = Double.parseDouble(type_amount.getText().toString());
+                    break;
+
+                default:
+                    break;
 //            case R.id.cloth_btn:
 //                new Thread()
 //                {
@@ -104,6 +427,11 @@ public class AccountingActivity extends Activity implements View.OnClickListener
 //                break;
 
 
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "用户非法操作", Toast.LENGTH_SHORT).show();
+            clear();
         }
 
 //        mHandler = new Handler()
@@ -119,6 +447,11 @@ public class AccountingActivity extends Activity implements View.OnClickListener
 //        };
 
 
+    }
+
+    private void clear() {
+        type_amount.setText("");
+        type_calculate.setText("");
     }
 
 
