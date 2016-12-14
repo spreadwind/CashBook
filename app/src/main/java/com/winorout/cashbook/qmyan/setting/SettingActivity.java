dd91c6f1964dd6dc0331993a36ced72857d158bd
wanxuedong <2381144912@qq.com>
2016-12-14 23:47:28 +0800

    记账功能更新，设置功能更新

--- app/src/main/java/com/winorout/cashbook/qmyan/setting/SettingActivity.java
+++ app/src/main/java/com/winorout/cashbook/qmyan/setting/SettingActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.winorout.cashbook.R;
 import com.winorout.cashbook.qmyan.personcenter.PersonCenter;
@@ -17,6 +19,9 @@ public class SettingActivity extends Activity implements View.OnClickListener {
     private TextView title;
     private ImageView back;
     private LinearLayout personCenter;
+    private LinearLayout help_layout;
+    private LinearLayout update;
+    private LinearLayout about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,9 @@ public class SettingActivity extends Activity implements View.OnClickListener {
     private void initEvent() {
         back.setOnClickListener(this);
         personCenter.setOnClickListener(this);
+        help_layout.setOnClickListener(this);
+        update.setOnClickListener(this);
+        about.setOnClickListener(this);
     }
 
     private void initView() {
@@ -38,6 +46,9 @@ public class SettingActivity extends Activity implements View.OnClickListener {
         title.setText("设置");
         back = (ImageView) findViewById(R.id.back);
         personCenter = (LinearLayout) findViewById(R.id.person_center);
+        help_layout = (LinearLayout)findViewById(R.id.help_layout);
+        update = (LinearLayout) findViewById(R.id.update_btn);
+        about = (LinearLayout)findViewById(R.id.about_btn);
     }
 
     @Override
@@ -48,6 +59,17 @@ public class SettingActivity extends Activity implements View.OnClickListener {
                 break;
             case R.id.person_center:
                 startActivity(new Intent(this, PersonCenter.class));
+                finish();
+                break;
+            case R.id.help_layout:
+                startActivity(new Intent(this,HelpActivity.class));
+                finish();
+                break;
+            case R.id.update_btn:
+                Toast.makeText(this,"当前已经是最新版",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about_btn:
+                startActivity(new Intent(this,AboutActivity.class));
                 finish();
                 break;
         }
